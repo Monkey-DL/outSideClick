a5252fb39095db600057d9102bff3b5b5e9130f4
Sasha <tokkars@mail.ru>
2020-05-15 00:16:27 +0300

    Update useOutsideClick.js

--- useOutsideClick.js
+++ useOutsideClick.js
@@ -1,3 +1,4 @@
+//function
 import { useEffect } from 'react';
 
 const useOutsideClick = (ref, callback) => {
@@ -21,3 +22,42 @@ export default useOutsideClick;
 // useOutsideClick(DropdownRef, () => { //DropdownRef = useref()
 //   toggle(false);
 // });
+
+
+//class
+export default class OutsideAlerter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.setWrapperRef = this.setWrapperRef.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside);
+  }
+
+  /**
+   * Set the wrapper ref
+   */
+  setWrapperRef(node) {
+    this.wrapperRef = node;
+  }
+
+  /**
+   * Alert if clicked on outside of element
+   */
+  handleClickOutside(event) {
+    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+      alert('You clicked outside of me!');
+    }
+  }
+
+  render() {
+    return <div ref={this.setWrapperRef}>{this.props.children}</div>;
+  }
+}
